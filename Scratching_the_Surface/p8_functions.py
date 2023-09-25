d2299d729d19e0b5a56dcf72997f592030eed761
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-09-26 04:01:58 +0530

    Create p8_functions.py

---
+++ Scratching_the_Surface/p8_functions.py
@@ -0,0 +1,13 @@
+def function():
+    print("Hello")
+    print("Bye")
+    #Identation is important in python
+
+#function call
+function()
+
+def greet(name):
+    print(f"HEllo {name}")
+    print("Hope you are doing good?")
+
+greet("Vishu")
